708a98790bcad65f6d170178f58124ae7bb8aab2
Stanley Umeanozie <coolstanlee@gmail.com>
2018-12-27 01:40:30 +0100

    Server setup

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
-app.listen(port, function(){
-  console.log('It\'s running...');
-});
 
 app.get('/', function(req, res){
-  res.send('LOL');
+  // res.send('LOL');
+  res.render('home');
+});
+
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+// Path where view files are saved
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.listen(port, function(){
+  console.log('It\'s running...');
 });
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// const path = require('path');
+// const webpack = require('webpack');
+//
+//
+// module.exports = {
+//
+//   mode: 'development',
+//   devServer: {
+//     inline: true,
+//     contentBase: './public',
+//     host: 'localhost',
+//     port: 3000,
+//     disableHostCheck: true
+//   },
+//   entry: './index.js',
+//   output: './public/js/app.min.js'
+//
+//
+//
+// }
